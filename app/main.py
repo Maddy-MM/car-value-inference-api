a2a0b0d03091000f689693f37322c9b797fd5bf7
Madhav Makwana <madhavmakwana.mm@gmail.com>
2026-04-10 14:23:36 +0530

    Refactor main application setup

--- app/main.py
+++ app/main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
-from app.api import routes_auth, routes_predict
+from app.api import routes_auth, routes_predict, routes_home, routes_health
 from app.middleware.logging_middleware import LoggingMiddleware
 from app.core.exceptions import register_exception_handlers
 
 app = FastAPI(title='Car Price Prediction API')
 
-# Link middleware
+# Middleware
 app.add_middleware(LoggingMiddleware)
 
-# Link endpoints
+# Routes
+app.include_router(routes_home.router, tags=['Home'])
+app.include_router(routes_health.router, tags=['Health'])
 app.include_router(routes_auth.router, tags=['Authorization'])
 app.include_router(routes_predict.router, tags=['Prediction'])
 
 # Monitoring using Prometheus
 Instrumentator().instrument(app).expose(app)
 
-# Add exception handler
+# Exception handlers
 register_exception_handlers(app)
