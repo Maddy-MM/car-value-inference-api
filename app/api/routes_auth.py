1c64d849053d2046558bfa30c4c4de2533a8ddbc
Madhav Makwana <madhavmakwana.mm@gmail.com>
2026-04-10 14:43:48 +0530

    update auth routes

--- app/api/routes_auth.py
+++ app/api/routes_auth.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+import os
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.core.security import create_token
 
 router = APIRouter()
+
+AUTH_USERNAME = os.getenv("AUTH_USERNAME")
+AUTH_PASSWORD = os.getenv("AUTH_PASSWORD")
 
 
 class AuthInput(BaseModel):
@@ -12,7 +16,7 @@ class AuthInput(BaseModel):
 
 @router.post("/login")
 def login(auth: AuthInput):
-    if (auth.username == "admin" and auth.password == "admin"):
+    if auth.username == AUTH_USERNAME and auth.password == AUTH_PASSWORD:
         token = create_token({"sub": auth.username})
         return {"access_token": token}
-    return {"error": "Invalid Credentials!"}
+    raise HTTPException(status_code=401, detail="Invalid Credentials!")
