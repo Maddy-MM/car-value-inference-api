2e97cbb5f47442080932a9e83b5b4e9511d0fc5f
Madhav Makwana <madhavmakwana.mm@gmail.com>
2026-04-10 14:23:34 +0530

    Update configuration settings

--- app/core/config.py
+++ app/core/config.py
@@ -4,13 +4,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class Settings():
-    PROJECT_NAME="Car Price Prediction API"
-    API_KEY=os.getenv("API_KEY")
-    JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY")
-    JWT_ALGORITHM="HS256"
-    REDIS_URL=os.getenv("REDIS_URL")
-    MODEL_PATH="app/models/model.joblib"
+class Settings:
+    PROJECT_NAME = "Car Price Prediction API"
+
+    # Auth credentials 
+    AUTH_USERNAME = os.getenv("AUTH_USERNAME")
+    AUTH_PASSWORD = os.getenv("AUTH_PASSWORD")
+
+    # Configs
+    API_KEY = os.getenv("API_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM = "HS256"
+    REDIS_URL = os.getenv("REDIS_URL")
+
+    # Paths
+    MODEL_PATH = "app/models/model.joblib"
 
 
 settings = Settings()
